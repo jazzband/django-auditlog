cc393c9611e8347979fa173261eb5bafd88cd701
Jan-Jelle Kester <janjelle@jjkester.nl>
2013-10-21 23:08:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django-auditlog',
+    version='0.1.0',
+    packages=['auditlog',],
+    package_dir={'': 'src'},
+    url='https://github.com/jjkester/django-auditlog',
+    license='MIT',
+    author='Jan-Jelle Kester',
+    author_email='janjelle@jjkester.nl',
+    description='Audit log app for Django'
+)
