5dc3275de9e855e975a6db3be03c3d054bd62cb3
Jan-Jelle Kester <janjelle@jjkester.nl>
2014-07-08 17:01:51 +0200

    Bump version number and copyright year

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog',
-    version='0.2.0',
+    version='0.2.1',
     packages=['auditlog',],
     package_dir={'': 'src'},
     url='https://github.com/jjkester/django-auditlog',
