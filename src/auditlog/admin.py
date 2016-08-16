3aeab92b3590e8ce870f68243d709597b662ea0f
Nitesh Rijal <rijal.it@gmail.com>
2016-08-16 18:58:06 +0545

    added admin filter by content_types

--- src/auditlog/admin.py
+++ src/auditlog/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import LogEntry
 
 class LogEntryAdmin(admin.ModelAdmin):
-        list_display = ('object_pk', 'object_repr', 'actor', 'action', 'changes', 'timestamp')
+    list_display = ('object_pk', 'object_repr', 'actor', 'action', 'changes', 'timestamp')
+    list_filter = ('content_type',)
 
 admin.site.register(LogEntry, LogEntryAdmin)
 
