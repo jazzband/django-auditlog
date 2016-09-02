20eb91359a427a8d81ab2bb470e9c154ed8a6915
Rob Guttman <rob@vook.com>
2016-09-02 15:02:42 -0400

    enhanced admin interface closes #1

--- src/auditlog/admin.py
+++ src/auditlog/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import LogEntry
+from .mixins import LogEntryAdminMixin
+from .filters import ResourceTypeFilter
 
-class LogEntryAdmin(admin.ModelAdmin):
-    list_display = ('object_pk', 'object_repr', 'actor', 'action', 'changes', 'timestamp')
-    list_filter = ('content_type',)
+
+class LogEntryAdmin(admin.ModelAdmin, LogEntryAdminMixin):
+    list_display = ['created', 'resource_url', 'action', 'msg_short', 'user_url']
+    search_fields = ['timestamp', 'object_repr', 'changes', 'actor__full_name']
+    list_filter = ['action', ResourceTypeFilter]
+    readonly_fields = ['created', 'resource_url', 'action', 'user_url', 'msg']
+    fieldsets = [
+        (None, {'fields': ['created', 'user_url', 'resource_url']}),
+        ('Changes', {'fields': ['action', 'msg']}),
+    ]
+
 
 admin.site.register(LogEntry, LogEntryAdmin)
-
---
+++ src/auditlog/mixins.py
@@ -0,0 +1,56 @@
+import json
+
+from django.conf import settings
+from django.core import urlresolvers
+
+MAX = 75
+
+
+class LogEntryAdminMixin(object):
+
+    def created(self, obj):
+        return obj.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    created.short_description = 'Created'
+
+    def user_url(self, obj):
+        if obj.actor:
+            app_label, model = settings.AUTH_USER_MODEL.split('.')
+            viewname = 'admin:%s_%s_change' % (app_label, model.lower())
+            link = urlresolvers.reverse(viewname, args=[obj.actor.id])
+            return u'<a href="%s">%s</a>' % (link, obj.actor.full_name or obj.actor.email)
+        return 'system'
+    user_url.allow_tags = True
+    user_url.short_description = 'User'
+
+    def resource_url(self, obj):
+        app_label, model = obj.content_type.app_label, obj.content_type.model
+        viewname = 'admin:%s_%s_change' % (app_label, model)
+        link = urlresolvers.reverse(viewname, args=[obj.object_id])
+        return u'<a href="%s">%s</a>' % (link, obj.object_repr)
+    resource_url.allow_tags = True
+    resource_url.short_description = 'Resource'
+
+    def msg_short(self, obj):
+        if obj.action == 2:
+            return ''  # delete
+        changes = json.loads(obj.changes)
+        s = '' if len(changes) == 1 else 's'
+        fields = ', '.join(changes.keys())
+        if len(fields) > MAX:
+            i = fields.rfind(' ', 0, MAX)
+            fields = fields[:i] + ' ..'
+        return '%d change%s: %s' % (len(changes), s, fields)
+    msg_short.short_description = 'Changes'
+
+    def msg(self, obj):
+        if obj.action == 2:
+            return ''  # delete
+        changes = json.loads(obj.changes)
+        msg = '<table><tr><th>#</th><th>Field</th><th>From</th><th>To</th></tr>'
+        for i, field in enumerate(sorted(changes), 1):
+            value = [i, field] + (['***', '***'] if field == 'password' else changes[field])
+            msg += '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % tuple(value)
+        msg += '</table>'
+        return msg
+    msg.allow_tags = True
+    msg.short_description = 'Changes'
