61da565355756577495c35eea035bcac4c669a0a
Ryan Castner <castner.rr@gmail.com>
2017-09-06 16:35:12 -0400

    Remove unneeded if check, the default argument to get provides the same fallback

--- src/auditlog/diff.py
+++ src/auditlog/diff.py
@@ -135,10 +135,7 @@ def model_instance_diff(old, new):
         new_value = get_field_value(new, field)
 
         if old_value != new_value:
-            if model_fields:
-                diff[model_fields['mapping_fields'].get(field.name, field.name)] = (smart_text(old_value), smart_text(new_value))
-            else:
-                diff[field.name] = (smart_text(old_value), smart_text(new_value))
+            diff[model_fields['mapping_fields'].get(field.name, field.name)] = (smart_text(old_value), smart_text(new_value))
 
     if len(diff) == 0:
         diff = None
