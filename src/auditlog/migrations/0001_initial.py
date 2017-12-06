3bfb3dd432c48d249a1b6cd547a179b7d32df1d2
Rod Manning <rod.t.manning@gmail.com>
2017-12-07 07:13:26 +1100

    Add 'on_delete' kwarg to initial migration
    Added and re-arranged 'on_delete' and 'to' kwargs in initial migration
    to ensure compatbility with later versions of Django.
    
    Also included updated manifest with changes required due to django 2.0
    work.
    
    Signed-off-by: Rod Manning <rod.t.manning@gmail.com>

--- src/auditlog/migrations/0001_initial.py
+++ src/auditlog/migrations/0001_initial.py
@@ -24,8 +24,8 @@ class Migration(migrations.Migration):
                 ('action', models.PositiveSmallIntegerField(verbose_name='action', choices=[(0, 'create'), (1, 'update'), (2, 'delete')])),
                 ('changes', models.TextField(verbose_name='change message', blank=True)),
                 ('timestamp', models.DateTimeField(auto_now_add=True, verbose_name='timestamp')),
-                ('actor', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, verbose_name='actor', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
-                ('content_type', models.ForeignKey(related_name='+', verbose_name='content type', to='contenttypes.ContentType')),
+                ('actor', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, related_name='+', verbose_name='actor', blank=True, null=True)),
+                ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType', related_name='+', verbose_name='content type')),
             ],
             options={
                 'ordering': ['-timestamp'],
