9152d225bb556d058d1c7f853d6060083d4b54ce
Alieh Rymašeŭski <alieh.rymasheuski@gmail.com>
2019-05-10 17:55:00 +0300

    Remove obsolete versions from setup and tox

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
