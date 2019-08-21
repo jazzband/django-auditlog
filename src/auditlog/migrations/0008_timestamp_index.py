e60876ae14a0c38640d65aeb35e699aab4674f91
Alieh Rymašeŭski <alieh.rymasheuski@gmail.com>
2019-08-21 19:30:25 +0300

    Add index for timestamp

---
+++ src/auditlog/migrations/0008_timestamp_index.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auditlog', '0007_object_pk_type'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='logentry',
+            name='timestamp',
+            field=models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='timestamp'),
+        ),
+    ]
--- src/auditlog/models.py
+++ src/auditlog/models.py
@@ -179,7 +179,7 @@ class LogEntry(models.Model):
     changes = models.TextField(blank=True, verbose_name=_("change message"))
     actor = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True, related_name='+', verbose_name=_("actor"))
     remote_addr = models.GenericIPAddressField(blank=True, null=True, verbose_name=_("remote address"))
-    timestamp = models.DateTimeField(auto_now_add=True, verbose_name=_("timestamp"))
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name=_("timestamp"))
     additional_data = JSONField(blank=True, null=True, verbose_name=_("additional data"))
 
     objects = LogEntryManager()
