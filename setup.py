07b38a934565da48efda56e49876ce83db18d49a
Alieh Rymašeŭski <alieh.rymasheuski@gmail.com>
2019-08-22 16:21:10 +0300

    Update references to the maintainer

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     version='0.5.3',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
-    url='https://github.com/jjkester/django-auditlog',
+    url='https://github.com/MacmillanPlatform/django-auditlog/',
     license='MIT',
     author='Jan-Jelle Kester',
+    maintainer='Alieh Rymašeŭski',
     description='Audit log app for Django',
     install_requires=[
         'django-jsonfield>=1.0.0',
