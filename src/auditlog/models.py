df2bf0a05c2c687b8865af86ffdac7fb1a575fd8
Alieh Rymašeŭski <alieh.rymasheuski@gmail.com>
2019-08-22 17:48:57 +0300

    Replace timestamp index with timestamp+id index
    This change is supposed to improve performance of admin view.

--- src/auditlog/models.py
+++ src/auditlog/models.py
@@ -176,7 +176,7 @@ class LogEntry(models.Model):
     changes = models.TextField(blank=True, verbose_name=_("change message"))
     actor = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True, related_name='+', verbose_name=_("actor"))
     remote_addr = models.GenericIPAddressField(blank=True, null=True, verbose_name=_("remote address"))
-    timestamp = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name=_("timestamp"))
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name=_("timestamp"))
     additional_data = JSONField(blank=True, null=True, verbose_name=_("additional data"))
 
     objects = LogEntryManager()
@@ -186,6 +186,7 @@ class LogEntry(models.Model):
         ordering = ['-timestamp']
         verbose_name = _("log entry")
         verbose_name_plural = _("log entries")
+        index_together = (("timestamp", "id"))
 
     def __str__(self):
         if self.action == self.Action.CREATE:
