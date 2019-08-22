5ba554af56858bb5e2427c3fca4da1d5eff986ba
Alieh Rymašeŭski <alieh.rymasheuski@gmail.com>
2019-08-22 18:06:48 +0300

    Bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog',
-    version='0.6.0',
+    version='0.6.1',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
     url='https://github.com/MacmillanPlatform/django-auditlog/',
