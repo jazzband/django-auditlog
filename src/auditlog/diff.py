2a75517667e8e6b924ba69288e083b516f758a40
Eugene Venter <eugene@catalyst.net.nz>
2019-11-28 10:51:13 +1300

    make field value retrieval slightly more robust
    when no field 'default' attribute is present
    
    credit to: https://github.com/stherrien
    
    Fixes #169

--- src/auditlog/diff.py
+++ src/auditlog/diff.py
@@ -77,7 +77,7 @@ def get_field_value(obj, field):
         try:
             value = smart_text(getattr(obj, field.name, None))
         except ObjectDoesNotExist:
-            value = field.default if field.default is not NOT_PROVIDED else None
+            value = field.default if getattr(field, 'default', NOT_PROVIDED) is not NOT_PROVIDED else None
 
     return value
 
