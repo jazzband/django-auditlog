ca29848d7857fffcfc1c3b12ede49fd371a8d0b5
Alieh Rymašeŭski <alieh.rymasheuski@gmail.com>
2020-11-12 12:31:23 +0300

    Remove compatibility code for old Django versions

--- src/auditlog_tests/urls.py
+++ src/auditlog_tests/urls.py
@@ -1,13 +1,7 @@
-import django
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 
-if django.VERSION < (1, 9):
-    admin_urls = include(admin.site.urls)
-else:
-    admin_urls = admin.site.urls
-
 urlpatterns = [
-    url(r'^admin/', admin_urls),
+    url(r'^admin/', admin.site.urls),
 ]
