bfd8d9d5c4b9ca282c22df0e2e5caf49dda69d49
Jannis Leidel <jannis@leidel.info>
2020-11-25 22:34:13 +0100

    Fixing postgres config?

--- auditlog_tests/test_settings.py
+++ auditlog_tests/test_settings.py
@@ -29,10 +29,10 @@ MIDDLEWARE = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('TEST_DB_NAME', 'postgres'),
+        'NAME': os.getenv('TEST_DB_NAME', 'auditlog_tests_db'),
         'USER': os.getenv('TEST_DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('TEST_DB_PASS', 'postgres'),
-        'HOST': os.getenv('TEST_DB_HOST', 'postgres'),
+        'PASSWORD': os.getenv('TEST_DB_PASS', ''),
+        'HOST': os.getenv('TEST_DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('TEST_DB_PORT', '5432'),
     }
 }
