d77803b3b48b83c396929d7aed6653c976ed470c
Hasan Ramezani <hasan.r67@gmail.com>
2020-12-07 21:11:01 +0100

    Add Django supported versions to setup.py classifiers.

--- setup.py
+++ setup.py
@@ -31,6 +31,10 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
         "License :: OSI Approved :: MIT License",
     ],
 )
