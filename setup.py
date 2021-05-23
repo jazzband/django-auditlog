0b751541917556b0a90116cf5cd1cf5bb3f0a2af
Hasan Ramezani <hasan.r67@gmail.com>
2021-05-23 23:09:40 +0200

    Remove Python 3.5 support.

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
