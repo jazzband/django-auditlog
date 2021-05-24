9ea948398bac091a23b3dd72772e03ca5ce0b46b
Hasan Ramezani <hasan.r67@gmail.com>
2021-05-24 23:20:14 +0200

    Remove Django 3.0 support.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
         "Framework :: Django :: 3.1",
         "License :: OSI Approved :: MIT License",
     ],
