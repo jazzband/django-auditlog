af86605a9098c892c38035bec012147c74036be6
Javier Tejero <javier.tejero@travelperk.com>
2021-07-29 16:58:13 +0200

    replace problematic django-jsonfield

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Jan-Jelle Kester',
     description='Audit log app for Django',
     install_requires=[
-        'django-jsonfield>=1.0.0',
+        'jsonfield>=1.0.0',
         'python-dateutil>=2.6.0,<2.8'
     ],
     zip_safe=False,
