ae608327171ef801926be70df64524ae54185f4a
Andreas Hasenkopf <crazyscientist@users.noreply.github.com>
2022-01-04 10:06:45 +0100

    Add a DB index to `LogEntry`'s `action` field (#236)
    For some applications the possibility to filter by the `action` field might be really interesting. However the lack of an index can lead to severe reduction of such queries.
    
    The simplest solution: Let's a DB index on that field :)

---
+++ auditlog/migrations/0010_action_index.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("auditlog", "0009_timestamp_id_index"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="logentry",
+            name="action",
+            field=models.PositiveSmallIntegerField(
+                choices=[(0, "create"), (1, "update"), (2, "delete")],
+                db_index=True,
+                verbose_name="action",
+            ),
+        ),
+    ]
