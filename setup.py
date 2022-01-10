bf9948a46ff8eadcd0b97306e186681e159a611c
Hasan Ramezani <hasan.r67@gmail.com>
2022-01-10 09:29:59 +0100

    Add Django4.0 to test pipeline.

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Framework :: Django",
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "License :: OSI Approved :: MIT License",
     ],
 )
