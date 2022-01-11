fe740713ae4a836b61f4aa13d2c2740853cc5138
Hasan Ramezani <hasan.r67@gmail.com>
2022-01-11 11:06:00 +0100

    Fix black error.

--- auditlog/apps.py
+++ auditlog/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AuditlogConfig(AppConfig):
     name = "auditlog"
     verbose_name = "Audit log"
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = "django.db.models.AutoField"
