f938a23839cd75bae7f574eaef6c834b60a66558
Hasan Ramezani <hasan.r67@gmail.com>
2022-11-14 18:53:28 +0100

    Replace `pkg_resources` with `importlib` solution

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 from datetime import date
 
-from pkg_resources import get_distribution
+from auditlog import __version__
 
 # -- Path setup --------------------------------------------------------------
 
@@ -33,7 +33,7 @@ project = "django-auditlog"
 author = "Jan-Jelle Kester and contributors"
 copyright = f"2013-{date.today().year}, {author}"
 
-release = get_distribution("django-auditlog").version
+release = __version__
 # for example take major/minor
 version = ".".join(release.split(".")[:2])
 
