d0f5a4fc618114542b6459d64118beb6896ec2e7
Abdullah Alaqeel <abdullah.t.aqeel@gmail.com>
2022-12-21 16:57:43 +0300

    docs: added docs for cid

--- auditlog/cid.py
+++ auditlog/cid.py
@@ -9,6 +9,15 @@ correlation_id = ContextVar("auditlog_correlation_id", default=None)
 
 
 def set_cid(request: Optional[HttpRequest] = None) -> None:
+    """
+    A function to read the cid from a request.
+    If the header is not in the request, then we set it to `None`.
+
+    Note: we look for the header in `request.headers` and `request.META`.
+
+    :param request: The request to get the cid from.
+    :return: None
+    """
     cid = None
     header = settings.AUDITLOG_CID_HEADER
 
@@ -25,6 +34,22 @@ def _get_cid() -> Optional[str]:
 
 
 def get_cid() -> Optional[str]:
+    """
+    Calls the cid getter function based on `settings.AUDITLOG_CID_GETTER`
+
+    If the setting value is:
+
+    * None: then it calls the default getter (which retrieves the value set in `set_cid`)
+    * callable: then it calls the function
+    * type(str): then it imports the function and then call it
+
+    The result is then returned to the caller.
+
+    If your custom getter does not depend on `set_header()`,
+    then we recommend setting `settings.AUDITLOG_CID_GETTER` to `None`.
+
+    :return: The correlation ID
+    """
     method = settings.AUDITLOG_CID_GETTER
     if not method:
         return _get_cid()
