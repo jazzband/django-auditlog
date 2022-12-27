ad49d458bf2c945c0c029ea1f774badd562cd89d
august <sum-rock@rockyhillhouse.org>
2022-12-27 14:08:41 -0600

    Update serial order of migrations

--- auditlog/migrations/0015_alter_logentry_changes.py
+++ auditlog/migrations/0015_alter_logentry_changes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("auditlog", "0010_alter_logentry_timestamp"),
+        ("auditlog", "0014_logentry_cid"),
     ]
 
     operations = [
