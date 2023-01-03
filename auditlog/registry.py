7f8422a206ac70366b894492e0dad416b7df5de5
Hasan Ramezani <hasan.r67@gmail.com>
2023-01-03 09:03:35 +0100

    Update auditlog/registry.py

--- auditlog/registry.py
+++ auditlog/registry.py
@@ -264,7 +264,8 @@ class AuditlogModelRegistry:
                 appmodel = self._get_model_classes(model["model"])
                 if not appmodel:
                     raise AuditLogRegistrationError(
-                        f"An error was encountered while registering model {model['model']} - make sure the app is registered correctly."
+                        f"An error was encountered while registering model '{model['model']}' - "
+                        "make sure the app is registered correctly."
                     )
                 model["model"] = appmodel[0]
                 self.unregister(model["model"])
