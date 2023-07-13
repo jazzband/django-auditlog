2c595f174ccd7bb251c8363ae115d7cd2ab817a0
Hasan Ramezani <hasan.r67@gmail.com>
2023-07-13 23:56:08 +0200

    Drop support for Python 3.7 (#546)

--- auditlog/__init__.py
+++ auditlog/__init__.py
@@ -1,15 +1,3 @@
-try:
-    from importlib.metadata import version  # New in Python 3.8
-except ImportError:
-    from pkg_resources import (  # from setuptools, deprecated
-        DistributionNotFound,
-        get_distribution,
-    )
+from importlib.metadata import version
 
-    try:
-        __version__ = get_distribution("django-auditlog").version
-    except DistributionNotFound:
-        # package is not installed
-        pass
-else:
-    __version__ = version("django-auditlog")
+__version__ = version("django-auditlog")
