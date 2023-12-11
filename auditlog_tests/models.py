dbb50e462c21d17cf2ce649bf6e572d59e7e0938
Aleh Rymašeŭski <alieh.rymasheuski@gmail.com>
2023-12-11 18:15:34 -0100

    Handle ObjectDoesNotExist in evaluation of repr (#592)

--- auditlog_tests/models.py
+++ auditlog_tests/models.py
@@ -86,6 +86,9 @@ class RelatedModel(RelatedModelParent):
 
     history = AuditlogHistoryField(delete_related=True)
 
+    def __str__(self):
+        return f"RelatedModel #{self.pk} -> {self.related.id}"
+
 
 class ManyRelatedModel(models.Model):
     """
