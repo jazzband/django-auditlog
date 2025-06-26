24d46a276c894ffe51aa09d7ab3f998207ecc70b
Mahdi Rahimi <31624047+mahdirahimi1999@users.noreply.github.com>
2025-06-26 18:25:22 +0330

    Update setup.py
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.9",
-    install_requires=["Django>=4.2,<5.3", "python-dateutil>=2.7.0"],
+    install_requires=["Django>=4.2", "python-dateutil>=2.7.0"],
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
