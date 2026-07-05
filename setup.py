80ac4805df8db51e1833790b942be87b7e3717d6
Youngkwang Yang <me@youngkwang.dev>
2026-07-06 01:16:19 +0900

    Add Python 3.14 and Django 6.0 to support matrix

--- setup.py
+++ setup.py
@@ -38,11 +38,13 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Framework :: Django",
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
         "Framework :: Django :: 5.2",
+        "Framework :: Django :: 6.0",
         "License :: OSI Approved :: MIT License",
     ],
 )
